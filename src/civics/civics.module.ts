518b308b4ab19ebcb59fb81dd9bd1f39eb372145
MindNest Dev <mindnest@example.com>
2026-03-10 23:02:27 +0000

    fix-render-prisma-generate-and-adminGuard-DI

--- src/civics/civics.module.ts
+++ src/civics/civics.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CivicsController } from './civics.controller';
 import { CivicsService } from './civics.service';
 import { PrismaService } from '../prisma.service';
+import { AdminGuard } from '../auth/admin.guard';
 
 @Module({
     controllers: [CivicsController],
-    providers: [CivicsService, PrismaService],
+    providers: [CivicsService, PrismaService, AdminGuard],
 })
 export class CivicsModule { }
